853bb5b34d7977059e67b57d6f8c926205322d12
Raphael Schrempf <schrempfra@gmail.com>
2023-07-20 17:25:19 +0200

    add delete todo

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,12 @@ function App() {
   const [todoInput, setTodoInput] = useState('');
   const [idForTodo, setIdForTodo] = useState('');
 
+  function deleteTodo(id)
+  {
+    setTodos([...todos].filter(todo => todo.id !== id));
+  }
+
+
   function handleInput(event)
   {
     setTodoInput(
@@ -78,7 +84,7 @@ function App() {
                   </div>
                 </div>
                 <div className="flex items-center">
-                  <span>
+                  <span className="cursor-pointer" onClick={() => deleteTodo(todo.id)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                     </svg>
